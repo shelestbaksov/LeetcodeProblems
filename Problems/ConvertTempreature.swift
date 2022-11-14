e0adea0da15af793a342f059f7e28db58ef1e534
Leisan Latypova <latley90@gmail.com>
2022-11-14 16:55:54 +0300

    Solved problems 14/11/22

---
+++ Problems/ConvertTempreature.swift
@@ -0,0 +1,12 @@
+//
+//  ConvertTempreature.swift
+//  Problems
+//
+//  Created by Leysan Latypova on 14.11.2022.
+//
+
+import Foundation
+
+func convertTemperature(_ celsius: Double) -> [Double] {
+    return [celsius + 273.15, celsius * 1.80 + 32]
+}
