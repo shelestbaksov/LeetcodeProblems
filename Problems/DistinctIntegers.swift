86a497e98a414797410a3dc88e2983e9a61ef4cb
Leisan Latypova <latley90@gmail.com>
2023-01-30 12:05:23 +0300

    Solved problems 30/01/23

---
+++ Problems/DistinctIntegers.swift
@@ -0,0 +1,12 @@
+//
+//  DistinctIntegers.swift
+//  Problems
+//
+//  Created by Leysan Latypova on 30.01.2023.
+//
+
+import Foundation
+
+func distinctIntegers(_ n: Int) -> Int {
+    return n > 1 ? n - 1 : 1
+}
