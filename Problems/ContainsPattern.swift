30e1b89309da1437603094d75c9b50332ddbb9f3
Leisan Latypova <latley90@gmail.com>
2023-02-28 16:09:08 +0300

    Solved problems 28/02/23

---
+++ Problems/ContainsPattern.swift
@@ -0,0 +1,18 @@
+//
+//  ContainsPattern.swift
+//  Problems
+//
+//  Created by Leisan Latypova on 28.02.2023.
+//
+
+import Foundation
+
+func containsPattern(_ arr: [Int], _ m: Int, _ k: Int) -> Bool {
+    guard m * k <= arr.count else {
+        return false
+    }
+    
+    
+    
+    return true
+}
---
+++ Problems/MaxRepeating.swift
@@ -0,0 +1,28 @@
+//
+//  MaxRepeating.swift
+//  Problems
+//
+//  Created by Leisan Latypova on 28.02.2023.
+//
+
+import Foundation
+
+func maxRepeating(_ sequence: String, _ word: String) -> Int {
+    guard sequence.contains(word) else {
+        return 0
+    }
+    
+    var max = 1
+    
+    while true {
+        var array = Array(repeating: word, count: max)
+        
+        if sequence.contains(array.joined()) {
+            max += 1
+        } else {
+            return max - 1
+        }
+    }
+    
+    return max
+}
