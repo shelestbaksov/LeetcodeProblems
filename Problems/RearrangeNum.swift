cb6b675e0fc5023bed52821d7f6a29c72ba893e6
Leisan Latypova <latley90@gmail.com>
2023-03-07 14:42:47 +0300

    Solved problems 07/03/23

---
+++ Problems/RearrangeNum.swift
@@ -0,0 +1,24 @@
+//
+//  RearrangeNum.swift
+//  Problems
+//
+//  Created by Leisan Latypova on 07.03.2023.
+//
+
+import Foundation
+
+func splitNum(_ num: Int) -> Int {
+    var arr = Array(String(num)).sorted()
+    var str1 = ""
+    var str2 = ""
+    
+    for i in 0..<arr.count {
+        if (i % 2 == 0) {
+            str1 += String(arr[i])
+        } else {
+            str2 += String(arr[i])
+        }
+    }
+    
+    return Int(str1)! + Int(str2)!
+}
